efa83bf5328fff651a7b03dae756aa0765cdf962
Renard Bergson <renardrock@gmail.com>
2022-09-08 19:23:28 -0300

    getting and setting calendar

--- src/digitalClock.js
+++ src/digitalClock.js
@@ -1,3 +1,14 @@
+/* Calendar code */
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let date = new Date()
+
+document.querySelector('.weekDay').innerHTML = weekDays[date.getDay()]
+
+document.querySelector('.day').innerHTML = `${date.getDate()}/`
+document.querySelector('.month').innerHTML = `${date.getMonth() + 1}/`
+document.querySelector('.year').innerHTML = date.getFullYear()
+
+/* Clock code */
 const $hours = document.querySelector('.hours')
 const $minutes = document.querySelector('.minutes')
 const $seconds = document.querySelector('.seconds')
