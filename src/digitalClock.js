8531ba4f66a7cc44e45af04e76cc0ec3e3cce9a3
Renard Bergson <renardrock@gmail.com>
2022-09-08 19:32:35 -0300

    organizing code

--- src/digitalClock.js
+++ src/digitalClock.js
@@ -1,19 +1,5 @@
-/* Calendar code */
-const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-let date = new Date()
-
-document.querySelector('.weekDay').innerHTML = weekDays[date.getDay()]
-
-document.querySelector('.day').innerHTML = `${date.getDate()}/`
-document.querySelector('.month').innerHTML = `${date.getMonth() + 1}/`
-document.querySelector('.year').innerHTML = date.getFullYear()
-
-/* Clock code */
-const $hours = document.querySelector('.hours')
-const $minutes = document.querySelector('.minutes')
-const $seconds = document.querySelector('.seconds')
-
-function timeConvert(element) {
+/* Format convert function */
+function timeAndDateConvert(element) {
     if (element < 10) {
         element = '0' + element
     }
@@ -21,10 +7,25 @@ function timeConvert(element) {
     return element
 }
 
+/* Calendar code */
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let date = new Date()
+
+document.querySelector('.weekDay').innerHTML = weekDays[date.getDay()]
+
+document.querySelector('.day').innerHTML = timeAndDateConvert(date.getDate()) + '/'
+document.querySelector('.month').innerHTML = timeAndDateConvert(date.getMonth() + 1) + '/'
+document.querySelector('.year').innerHTML = date.getFullYear()
+
+/* Clock code */
+const $hours = document.querySelector('.hours')
+const $minutes = document.querySelector('.minutes')
+const $seconds = document.querySelector('.seconds')
+
 setInterval( () => {
     let today = new Date()
 
-    $hours.innerText = timeConvert(today.getHours())
-    $minutes.innerText = timeConvert(today.getMinutes())
-    $seconds.innerText = timeConvert(today.getSeconds())
+    $hours.innerText = timeAndDateConvert(today.getHours())
+    $minutes.innerText = timeAndDateConvert(today.getMinutes())
+    $seconds.innerText = timeAndDateConvert(today.getSeconds())
 }, 500)
