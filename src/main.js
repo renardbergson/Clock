298d044c072a13938fcada2221b75b800b7b3f66
Lincoln Araujo <lincoln.fa@hotmail.com>
2022-09-10 13:08:55 -0300

    setting workspace2 position

--- src/main.js
+++ src/main.js
@@ -54,17 +54,63 @@ let current_time = new Date().getHours()
 if (6 <= current_time && current_time < 12) {
 
     document.body.style.background = 'linear-gradient(270deg, #6BD3EB  0%, #2293B0 100%)'
-    document.querySelector('.leftButton').style.background = '#A3E9FF'
+   
 
 }else if (12 <= current_time && current_time < 18) {
 
     document.body.style.background = 'linear-gradient(270deg, #FEB78B 0%, #DE6362 100%)'
-    document.querySelector('.leftButton').style.background = '#FF4D00'
+   
 
 }else {
 
     document.body.style.background = 'linear-gradient(270deg, #243B55 0%, #141E30 100%)'
-    document.querySelector('.leftButton').style.background = '#4980E2'
+   
 }
 
 /* - - - - - - - - - - - - - - - - - - - - - analogic clock code - - - - - - - - - - - - - - - - - - */
+
+// Selecting all of the css classes on which
+// we want to apply functionalities
+const hr = document.querySelector('.hr');
+const min = document.querySelector('.min');
+const sec = document.querySelector('.sec');
+  
+// Setting up the period of working
+setInterval(() => {
+  
+    // Extracting the current time 
+    // from DATE() function
+    let day = new Date();
+    let hour = day.getHours();
+    let minutes = day.getMinutes();
+    let seconds = day.getSeconds();
+  
+    // Formula that is explained above for 
+    // the rotation of different hands
+    let hrrotation = (30 * hour) + (0.5 * minutes);
+    let minrotation = 6 * minutes;
+    let secrotation = 6 * seconds;
+  
+    hr.style.transform =
+        `translate(-50%,-100%) rotate(${hrrotation}deg)`
+    min.style.transform =
+        `translate(-50%,-100%) rotate(${minrotation}deg)`
+    sec.style.transform =
+        `translate(-50%,-85%) rotate(${secrotation}deg)`
+});
+
+
+let week = ["Monday", "Sunday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+let day = new Date();
+
+let month = day.toLocaleString("en-US", { month: "short" });
+
+let dayMonth = day.getDate();
+
+document.getElementById("day-of-the-week").innerHTML = week[day.getDay()];
+
+document.getElementById("month").innerHTML = month;
+
+document.getElementById("day-of-month").innerHTML = dayMonth;
